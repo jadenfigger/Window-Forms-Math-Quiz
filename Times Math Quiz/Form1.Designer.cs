baca494e2963c2bd1a14d00a35978d7565bd1bf4
jadenfigger <jadenfigger@gmail.com>
2021-05-13 14:11:35 -0700

    Add files via upload

---
+++ Times Math Quiz/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿
+namespace Times_Math_Quiz
+{
+    partial class form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.A = new System.Windows.Forms.Button();
+            this.C = new System.Windows.Forms.Button();
+            this.D = new System.Windows.Forms.Button();
+            this.B = new System.Windows.Forms.Button();
+            this.QuestionTextBox = new System.Windows.Forms.TextBox();
+            this.textCorrect = new System.Windows.Forms.TextBox();
+            this.textWrong = new System.Windows.Forms.TextBox();
+            this.QuestionTrackerText = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // A
+            // 
+            this.A.BackColor = System.Drawing.Color.DodgerBlue;
+            this.A.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.A.Font = new System.Drawing.Font("Segoe UI", 45F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.A.Location = new System.Drawing.Point(7, 300);
+            this.A.Name = "A";
+            this.A.Size = new System.Drawing.Size(720, 200);
+            this.A.TabIndex = 0;
+            this.A.Text = "button1";
+            this.A.UseVisualStyleBackColor = false;
+            this.A.Click += new System.EventHandler(this.Answer_Click);
+            // 
+            // C
+            // 
+            this.C.BackColor = System.Drawing.Color.DodgerBlue;
+            this.C.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.C.Font = new System.Drawing.Font("Segoe UI", 45F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.C.Location = new System.Drawing.Point(7, 506);
+            this.C.Name = "C";
+            this.C.Size = new System.Drawing.Size(720, 200);
+            this.C.TabIndex = 1;
+            this.C.Text = "button1";
+            this.C.UseVisualStyleBackColor = false;
+            this.C.Click += new System.EventHandler(this.Answer_Click);
+            // 
+            // D
+            // 
+            this.D.BackColor = System.Drawing.Color.DodgerBlue;
+            this.D.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.D.Font = new System.Drawing.Font("Segoe UI", 45F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.D.Location = new System.Drawing.Point(733, 506);
+            this.D.Name = "D";
+            this.D.Size = new System.Drawing.Size(720, 200);
+            this.D.TabIndex = 3;
+            this.D.Text = "button2";
+            this.D.UseVisualStyleBackColor = false;
+            this.D.Click += new System.EventHandler(this.Answer_Click);
+            // 
+            // B
+            // 
+            this.B.BackColor = System.Drawing.Color.DodgerBlue;
+            this.B.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.B.Font = new System.Drawing.Font("Segoe UI", 45F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.B.Location = new System.Drawing.Point(733, 300);
+            this.B.Name = "B";
+            this.B.Size = new System.Drawing.Size(720, 200);
+            this.B.TabIndex = 2;
+            this.B.Text = "button1";
+            this.B.UseVisualStyleBackColor = false;
+            this.B.Click += new System.EventHandler(this.Answer_Click);
+            // 
+            // QuestionTextBox
+            // 
+            this.QuestionTextBox.BackColor = System.Drawing.Color.LightGray;
+            this.QuestionTextBox.Font = new System.Drawing.Font("Segoe UI", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.QuestionTextBox.Location = new System.Drawing.Point(287, 27);
+            this.QuestionTextBox.MinimumSize = new System.Drawing.Size(777, 30);
+            this.QuestionTextBox.Multiline = true;
+            this.QuestionTextBox.Name = "QuestionTextBox";
+            this.QuestionTextBox.Size = new System.Drawing.Size(900, 250);
+            this.QuestionTextBox.TabIndex = 4;
+            this.QuestionTextBox.Text = "What is the square root of 144 times the square root of 256?";
+            this.QuestionTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textCorrect
+            // 
+            this.textCorrect.BackColor = System.Drawing.Color.LightGray;
+            this.textCorrect.Font = new System.Drawing.Font("Segoe UI", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textCorrect.Location = new System.Drawing.Point(13, 27);
+            this.textCorrect.Name = "textCorrect";
+            this.textCorrect.Size = new System.Drawing.Size(209, 63);
+            this.textCorrect.TabIndex = 5;
+            this.textCorrect.Text = "Correct:  0";
+            // 
+            // textWrong
+            // 
+            this.textWrong.BackColor = System.Drawing.Color.LightGray;
+            this.textWrong.Font = new System.Drawing.Font("Segoe UI", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textWrong.Location = new System.Drawing.Point(13, 96);
+            this.textWrong.Name = "textWrong";
+            this.textWrong.Size = new System.Drawing.Size(209, 63);
+            this.textWrong.TabIndex = 6;
+            this.textWrong.Text = "Wrong:  0";
+            // 
+            // QuestionTrackerText
+            // 
+            this.QuestionTrackerText.BackColor = System.Drawing.Color.LightGray;
+            this.QuestionTrackerText.Font = new System.Drawing.Font("Segoe UI", 23F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.QuestionTrackerText.Location = new System.Drawing.Point(1193, 27);
+            this.QuestionTrackerText.Name = "QuestionTrackerText";
+            this.QuestionTrackerText.Size = new System.Drawing.Size(260, 58);
+            this.QuestionTrackerText.TabIndex = 7;
+            this.QuestionTrackerText.Text = "Question:  0/9";
+            // 
+            // form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(1465, 711);
+            this.Controls.Add(this.QuestionTrackerText);
+            this.Controls.Add(this.textWrong);
+            this.Controls.Add(this.textCorrect);
+            this.Controls.Add(this.QuestionTextBox);
+            this.Controls.Add(this.D);
+            this.Controls.Add(this.B);
+            this.Controls.Add(this.C);
+            this.Controls.Add(this.A);
+            this.Name = "form1";
+            this.Text = "Timed Math Quiz";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button A;
+        private System.Windows.Forms.Button C;
+        private System.Windows.Forms.Button AnswerD;
+        private System.Windows.Forms.Button B;
+        private System.Windows.Forms.TextBox QuestionTextBox;
+        private System.Windows.Forms.Button D;
+        private System.Windows.Forms.TextBox textCorrect;
+        private System.Windows.Forms.TextBox textWrong;
+        private System.Windows.Forms.TextBox QuestionTrackerText;
+    }
+}
+
---
+++ Times Math Quiz/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Times_Math_Quiz
+{
+    public partial class form1 : Form
+    {
+        public string[] questions = new string[] { "An algebraic expression that contains three terms is called?",
+                                                   "A die is rolled. What is the probability of getting an even number?",
+                                                   "In a race , Ram covers 5 km in 20 min. How much distance will he cover in 100 min ?",
+                                                   "21 goats eat as much as 15 cows. How many goats eat as much as 35 cows?",
+                                                   "What is the 7th digit in Pi?",
+                                                   "What is the next prime number after 29?",
+                                                   "A billion comes before a trillion, but what comes after a trillion?",
+                                                   "What is the name of a 9 sided polygon",
+                                                   "What is 2 + 8 * 8 / 2"};
+        public string[,] answers = new string[,] { { "Monomial", "Binomial", "Trinomial", "None of these"},
+                                                   { "1/6", "1/2", "1/3", "1/4"},
+                                                   { "25km", "30km", "26km", "40km"},
+                                                   { "41", "37", "49", "38"},
+                                                   { "3", "5", "8", "2"},
+                                                   { "34", "31", "39", "33"},
+                                                   { "Quintillion", "Decillion", "Quadrillion", "Septillion"},
+                                                   { "Nonagon", "Decagon", "Heptagon", "Dodecagon"},
+                                                   { "38", "42", "40", "34"}};
+        public string[] correctAnswer = new string[] { "C", "B", "A", "C", "D", "B", "C", "A", "D"};
+        public int questionTime = 10;
+        public int questionNumber = 0;
+
+        public System.Windows.Forms.Timer myTimer = new System.Windows.Forms.Timer();
+        public bool timerRunning = false;
+
+        public int totalWrong = 0;
+        public int totalCorrect = 0;
+
+        public form1()
+        {
+            InitializeComponent();
+
+            DisplayQuestion();
+        }
+
+        public void DisplayQuestion()
+        {
+            QuestionTextBox.Text = questions[questionNumber];
+            A.Text = answers[questionNumber, 0];
+            B.Text = answers[questionNumber, 1];
+            C.Text = answers[questionNumber, 2];
+            D.Text = answers[questionNumber, 3];
+            QuestionTrackerText.Text = $"Question:  {questionNumber+1}/9";
+        }
+
+        private void Answer_Click(object sender, EventArgs e)
+        {
+            var button = (Button)sender;
+
+            if (button.Name == correctAnswer[questionNumber])
+            {
+                totalCorrect += 1;
+
+                if (questionNumber+1 == questions.Length)
+                {
+                    questionNumber = 0;
+                    totalWrong = 0;
+                    totalCorrect = 0;
+                } 
+                else
+                {
+                    questionNumber += 1;
+                }
+                DisplayQuestion();
+            }
+            else
+            {
+                totalWrong += 1;
+            }
+
+            textCorrect.Text = $"Correct:  {totalCorrect}";
+            textWrong.Text = $"Wrong:  {totalWrong}";
+        }
+    }
+}
